0269f715aed2353c7dd2f1b6b5989153f0e4dd73
kursk <kursk.ye@gmail.com>
2016-04-08 16:46:19 +0800

    add rodent main method

--- src/chapter/ten/rodent/Rodent.java
+++ src/chapter/ten/rodent/Rodent.java
@@ -1,6 +1,6 @@
 package chapter.ten.rodent;
 
-import  static kursk.Kutils.*;
+import static kursk.Kutils.*;
 
 /**
  * Created by kursk on 2016/4/1.
@@ -8,52 +8,63 @@ import  static kursk.Kutils.*;
 public class Rodent {
     private static RandomRodentGenerator rrg = new RandomRodentGenerator();
 
-    void dig(){
+    void dig() {
         print("Rodent dig");
     }
 
-    void eat(){
+    void eat() {
         print("Rodent eat");
     }
 
     public static void main(String[] args) {
         Rodent[] s = new Rodent[9];
         // Fill up the array with shapes:
-        for(int i = 0; i < s.length; i++)
+        for (int i = 0; i < s.length; i++)
             s[i] = rrg.next();
         // Make polymorphic method calls:
-        for(Rodent shp : s)
+        for (Rodent shp : s) {
             shp.dig();
+        }
+        if (0.1 + 0.2 == 0.3) {
+            print("equal");
+            ;
+        } else {
+            print("no equal");
+        }
+
     }
 }
 
- class Mouse extends Rodent {
-    void dig(){
+
+class Mouse extends Rodent {
+
+
+    void dig() {
         print("mouse dig");
     }
 
-    void eat(){
+    void eat() {
         print("mouse eat");
     }
 }
 
 class Gorbil extends Rodent {
 
-    void dig(){
+    void dig() {
         print("Gorbil dig");
     }
 
-    void eat(){
+    void eat() {
         print("Gorbil eat");
     }
 }
 
 class Hamster extends Rodent {
-    void dig(){
+    void dig() {
         print("Hamster dig");
     }
 
-    void eat(){
+    void eat() {
         print("Hamster eat");
     }
 }
