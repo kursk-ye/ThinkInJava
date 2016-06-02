6af9ea4b07f61e0b0983a3deb9e190176f9ab24e
kursk <kursk.ye@gmail.com>
2016-06-02 17:39:26 +0800

    add rodent main method

--- src/chapter/innerclass/anonymous/Callbacks.java
+++ src/chapter/innerclass/anonymous/Callbacks.java
@@ -52,13 +52,15 @@ class Caller {
 
 public class Callbacks {
     public static void main(String[] args) {
-        Callee1 c1 = new Callee1();
+/*        Callee1 c1 = new Callee1();
+        Caller caller1 = new Caller(c1);
+        caller1.go();
+        caller1.go();
+        caller1.go();*/
+
         Callee2 c2 = new Callee2();
         MyIncrement.f(c2);
-        Caller caller1 = new Caller(c1);
         Caller caller2 = new Caller(c2.getCallbackReference());
-        caller1.go();
-        caller1.go();
         caller2.go();
         caller2.go();
     }
