ed44b953ae5d409b7db7394643bb750975b1e26b
kursk.ye@gmail.com <kursk.ye@gmail.com>
2017-09-21 15:48:40 +0800

    I know about logconfigure

--- src/chapter/innerclass/page361/Factories.java
+++ src/chapter/innerclass/page361/Factories.java
@@ -1,7 +1,25 @@
 package chapter.innerclass.page361;
 
 /**
- * Created by yelei on 17-9-21.
+ * Created by kursk on 9/20/2017.
  */
+import static kursk.Kutils.*;
+
+interface Service {
+    void method1();
+    void method2();
+}
+
+interface ServiceFactory{
+    Service getService();
+}
+
+class Implementation1 implements Service {
+   private Implementation1(){}
+   public void method1(){println("Implementation1 method1");}
+    public void method2(){println("Implementation1 method2");}
+    public static Serv
+}
+
 public class Factories {
 }
