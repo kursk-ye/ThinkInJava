3d71d2e1675cc9e1ec4dc6d199fe2868cd518f9d
kursk.ye@gmail.com <kursk.ye@gmail.com>
2017-11-24 23:01:08 +0800

    Merge branch 'master' of https://github.com/kursk-ye/ThinkInJava
    # Conflicts:
    #	.idea/workspace.xml
    #	ThinkInJava.iml

--- src/chapter/holdyourobjects/exe17/Exe17.java
+++ src/chapter/holdyourobjects/exe17/Exe17.java
@@ -5,6 +5,7 @@ import chapter.holdyourobjects.exe1.Gerbil;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.TreeMap;
 
 import static kursk.Kutils.print;
 import static kursk.Kutils.println;
@@ -16,11 +17,13 @@ public class Exe17 {
     public static void main(String[] args) {
         String s;
 
-        Map<String , Gerbil> map = new HashMap<>();
+        Map<String , Gerbil> map = new TreeMap<>();
 
-        map.put("g"+ 1 , new Gerbil(1));
-        map.put("g"+ 2 , new Gerbil(2));
-        map.put("g"+ 3 , new Gerbil(3));
+        map.put("g"+ 7 , new Gerbil(1));
+        map.put("g"+ 3 , new Gerbil(2));
+        map.put("g"+ 5 , new Gerbil(3));
+        map.put("g"+ 6 , new Gerbil(3));
+        map.put("g"+ 2 , new Gerbil(3));
 
         Iterator<String> it = map.keySet().iterator();
 
--- src/chapter/holdyourobjects/exe31/RandomShapeGenerator31.java
+++ src/chapter/holdyourobjects/exe31/RandomShapeGenerator31.java
@@ -1,7 +1,60 @@
 package chapter.holdyourobjects.exe31;
 
+import chapter.polymorphism.shape.Circle;
+import chapter.polymorphism.shape.Shape;
+import chapter.polymorphism.shape.Square;
+import chapter.polymorphism.shape.Triangle;
+
+import java.util.Iterator;
+import java.util.Random;
+
+import static kursk.Kutils.println;
+
 /**
  * Created by kursk on 11/24/2017.
  */
-public class RandomShapeGenerator31 {
+public class RandomShapeGenerator31 implements Iterable<Shape> {
+    private Random rand = new Random();
+
+    public Shape make() {
+        switch(rand.nextInt(3)) {
+            default:
+            case 0: return new Circle();
+            case 1: return new Square();
+            case 2: return new Triangle();
+        }
+    }
+    private Shape[] shapes;
+
+    RandomShapeGenerator31(int n) {
+        shapes = new Shape[n];
+        for(int i = 0; i < n; i++)
+            shapes[i] = make();
+
+    }
+
+    public  Iterator<Shape> iterator(){
+        return new Iterator<Shape>() {
+            private int index = 0;
+            @Override
+            public boolean hasNext() {
+                return index < shapes.length;
+            }
+
+            @Override
+            public Shape next() {
+                return shapes[index++];
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        RandomShapeGenerator31 rsg = new RandomShapeGenerator31(20);
+        for (Shape s : rsg)
+            println(s);
+    }
 }
