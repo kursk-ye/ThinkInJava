451347a6b2c2c1b988b305b2a48ccbcaa86faa31
yelei <kursk.ye@gmail.com>
2017-11-24 12:55:22 +0800

    Merge branch 'master' of https://github.com/kursk-ye/ThinkInJava
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

--- src/chapter/holdyourobjects/exe22/Exe22.java
+++ src/chapter/holdyourobjects/exe22/Exe22.java
@@ -1,4 +1,68 @@
 package chapter.holdyourobjects.exe22;
 
+import net.mindview.util.TextFile;
+
+import java.util.*;
+
+import static kursk.Kutils.print;
+import static kursk.Kutils.println;
+
 public class Exe22 {
+
+    List<Vowel> getVowel(Set<String> words){
+        List<Vowel> vList = new ArrayList<Vowel>();
+        Set<Character> vowels = new TreeSet<Character>();
+
+        Collections.addAll(vowels, 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
+        for (String s : words){
+            Integer count = 0;
+            Vowel v = new Vowel();
+            for (Character c : s.toCharArray()) {
+                if (vowels.contains(c)) {
+                    count++;
+                }
+            }
+            v.setWord(s);
+            v.setCount(count);
+            vList.add(v);
+        }
+
+        return vList;
+    }
+
+    public static void main(String[] args) {
+        Set<String> words = new TreeSet<String>(new TextFile("MapOfList.java", "\\W+"));
+        List<Vowel> l = new ArrayList<Vowel>();
+
+        Exe22 e = new Exe22();
+        l = e.getVowel(words);
+
+        for(int i=0; i<l.size(); i++){
+            print(l.get(i).getWord() + ":");
+            println(l.get(i).getCount());
+        }
+
+    }
+
+    class Vowel{
+        private String word;
+        private Integer count;
+
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public Integer getCount() {
+            return count;
+        }
+
+        public void setCount(Integer count) {
+            this.count = count;
+        }
+    }
 }
