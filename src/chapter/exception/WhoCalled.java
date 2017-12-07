7ebf5c16b3bf745a30399814446e5274fad5cab4
kursk.ye@gmail.com <kursk.ye@gmail.com>
2017-12-07 23:50:22 +0800

    Merge branch 'master' of https://github.com/kursk-ye/ThinkInJava
    # Conflicts:
    #	.idea/workspace.xml
    #	ThinkInJava.iml

---
+++ src/chapter/exception/WhoCalled.java
@@ -0,0 +1,29 @@
+package chapter.exception;
+
+import static kursk.Kutils.println;
+
+/**
+ * Created by kursk on 12/7/2017.
+ */
+
+public class WhoCalled {
+    static void f() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            for (StackTraceElement ste : e.getStackTrace())
+                println(ste.getMethodName());
+        }
+    }
+    static void g(){ f(); }
+
+    static void h(){ g(); }
+
+    public static void main(String[] args) {
+        f();
+        println("--------------------");
+        g();
+        println("--------------------");
+        h();
+    }
+}
