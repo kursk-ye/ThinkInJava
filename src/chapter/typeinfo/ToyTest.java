350852d8f6191fc78299f144df9af4ad94ebdfa5
kursk.ye@gmail.com <kursk.ye@gmail.com>
2018-01-13 19:27:33 +0800

    Merge branch 'master' of https://github.com/kursk-ye/ThinkInJava
    # Conflicts:
    #	.idea/workspace.xml

--- src/chapter/typeinfo/ToyTest.java
+++ src/chapter/typeinfo/ToyTest.java
@@ -1,7 +1,28 @@
 package chapter.typeinfo;
 
+import static kursk.Kutils.println;
+
 /**
  * Created by kursk on 1/9/2018.
  */
+
+interface HasBatteries {}
+interface Waterproof {}
+interface Shoots {}
+
+class Toy {
+    Toy() {} // if not
+    Toy(int i) {}
+}
+
+class FancyToy extends Toy implements HasBatteries, Waterproof, Shoots {
+    FancyToy() {super(1);}
+}
+
+
 public class ToyTest {
+    static void printInfo(Class cc){
+        println("Class name:" + cc.getName() +
+                " is interface? [" + cc.isInterface() + "]");
+    }
 }
