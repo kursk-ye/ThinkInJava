fc78c63ed2a8bb6c0ed1530a44f89b8bb653b7e0
yelei <kursk.ye@gmail.com>
2018-06-11 14:47:23 +0800

    Merge branch 'master' of https://github.com/kursk-ye/ThinkInJava
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/workspace.xml

---
+++ src/kursk/topic36/TestInt.java
@@ -0,0 +1,10 @@
+package kursk.topic36;
+
+public class TestInt {
+    int i;
+
+    public static void main(String[] args) {
+        Integer integer = 1;
+        int unboxing = integer ++;
+    }
+}
